d93b5abc18bb3200c0f686af9c70213f04ab5696
Azizame <mahamatova0@gmail.com>
2023-10-31 13:04:02 +0500

    done

--- js/script.js
+++ js/script.js
@@ -22,3 +22,24 @@ function myAbout() {
     x.className = "fa fa-heart";
   }
 }
+function menuKatalog() {
+  var x = document.getElementById("menu");
+  var icon = document.getElementById("btn")
+  if (x.className === "menu col-lg-3 d-none") {
+    x.className = "menu col-lg-3 onPos d-flex";
+  } else {
+    x.className = "menu col-lg-3 d-none";
+  }
+  if (icon.className === "iconka") {
+    icon.className = "iconka close";
+  } else {
+    icon.className = "iconka";
+  }
+}
+window.addEventListener("scroll", function(){
+  let header = document.getElementById("header");
+  let section = document.getElementById("section")
+  console.log(header, "aziza");
+  header.classList.toggle("sticky", window.scrollY > 55)
+  section.classList.toggle("first-m", window.scrollY > 55)
+})
